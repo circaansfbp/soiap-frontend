7e2bbdf4a7202066d9aa164195dabc223375e625
circaansfbp <franciscambp@outlook.com>
2021-04-06 12:20:04 -0400

    diseño preliminar de agenda de horarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HorarioComponent } from './horario/horario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorarioComponent
   ],
   imports: [
     BrowserModule
