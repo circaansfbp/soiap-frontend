25c5c8ef5b1b13f590a0416d3d2b670d69d00a68
circaansfbp <franciscambp@outlook.com>
2021-04-06 13:01:51 -0400

    mejora en diseño, falta arreglar fecha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'soiap-frontend';
+  title = 'Sistema de organización de info. y atención psicológica';
+  fechaActual: Date = new Date();
 }
 
 
