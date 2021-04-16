ff925e55db87ccfe7f7b90501f435315ee23f2a2
circaansfbp <franciscambp@outlook.com>
2021-04-16 12:56:41 -0400

    mejora aspecto de la agenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -25,6 +25,7 @@ const routes = [
 
   imports: [
     BrowserModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot(routes)
--- src/app/components/hora-atencion/agenda/agenda.component.ts
+++ src/app/components/hora-atencion/agenda/agenda.component.ts
@@ -10,7 +10,7 @@ moment.locale('es');
   styleUrls: ['./agenda.component.css']
 })
 export class AgendaComponent implements OnInit {
-  titleDate = moment().format('dddd Do MMMM YYYY'); // Fecha actual para desplegar como título
+  titleDate = moment().format('[Hoy es ] dddd[, ] Do [de] MMMM[ de ] YYYY'); // Fecha actual para desplegar como título
   actualDate = moment().format("YYYY[-]M[-]D"); // Fecha a pasar al componente horario para búsquedas en BD
 
   changeDay: number = 0; // Variable que maneja la adición y sustracción de días 
@@ -35,5 +35,11 @@ export class AgendaComponent implements OnInit {
     let previous = moment().add(this.changeDay, 'd');
     this.actualDate = previous.format("YYYY[-]M[-]D");
     this.titleDate = previous.format('dddd Do MMMM YYYY');
+  }
+
+  // Buscar fecha específica mediante input type date
+  searchBySpecificDate(date: string) {
+    this.actualDate = moment(date).format("YYYY[-]M[-]D");
+    this.titleDate = moment(date).format('dddd Do MMMM YYYY');
   }
 }
